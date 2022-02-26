4f5d17b52e1a6c006b505580e23c0779a7d758bb
sozinho <soaresjonatas398@gmail.com>
2022-02-26 20:43:22 +0000

    Update

--- src/script.js
+++ src/script.js
@@ -13,15 +13,28 @@ $("#formulario").submit(function (e) {
 
 function calcularMediaParaSerRetirados(e) {
   mediaParaSerRetirados.innerText = 'Total para bater a meta: ' + Math.floor((Number(e.target.value) * 70) / 100);
+  console.log(retirados)
+  if (document.getElementById('retirados').value == '') return;
+  else calcularMediaRetirados(document.getElementById('retirados').value);
 }
+
 function calcularMediaRetirados(e) {
-  mediaRetirados.innerText = `${((Number(e.target.value) * 100) / total.value).toFixed(2)}% retirados`;
-  if (((Number(e.target.value) * 100) / total.value) >= 70) {
+  const valor = document.getElementById('retirados').value || e.target?.value
+  if (( valor * 100) / total.value == Infinity || (valor * 100) / total.value == NaN) {
+    mediaRetirados.style.color = 'red';
+    mediaRetirados.style.animation = 'flicker 1.5s infinite alternate';
+    return mediaRetirados.innerText = 'Algo está errado!';
+  }
+
+  mediaRetirados.innerText = `${((Number(valor) * 100) / total.value).toFixed(2)}% retirados`;
+  mediaRetirados.style.color = 'black';
+  mediaRetirados.style.animation = '';
+  if (((Number(valor) * 100) / total.value) >= 70) {
     success.style.opacity = '1';
     checkmark.style.width = '100px';
     checkmark.style.height = '100px';
   }
-  if (((Number(e.target.value) * 100) / total.value) <= 69) {
+  if (((Number(valor) * 100) / total.value) <= 69) {
     success.style.opacity = '0';
     checkmark.style.width = '0px';
     checkmark.style.height = '0px';
